821fee20a58a9f8f5cf9af558a225b949e08fc74
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-04-11 01:03:40 +0800

    添加日志

--- listener/changeOpenAiKey.py
+++ listener/changeOpenAiKey.py
@@ -1,6 +1,6 @@
 from config import conf
 from flask import Blueprint,jsonify
-
+from common.log import logger
 from dto.response import ResultEntity
 
 conf = conf()
@@ -12,7 +12,7 @@ blueprint = Blueprint('config', __name__,url_prefix='')
 @blueprint.route('/config/changeApiKey/<openKey>', methods=['GET'])
 def changeApiKey(openKey):
     conf['open_ai_api_key']=openKey
-    print("收到通知更改key",openKey)
+    logger.info("收到通知更改key",openKey)
     return jsonify(ResultEntity().to_dict())
 
 

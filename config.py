b5f2eaa2998ce3702423f0b4ecb6908733c06b57
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-04-28 11:12:16 +0800

    修改路径，解决在windows平台无法登陆

--- config.py
+++ config.py
@@ -170,10 +170,11 @@ def setOpenAiKey(newKey):
     config['open_ai_api_key'] = newKey
 def load_config():
     global config
-    config_path = "./config.json"
+    config_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(config_dir, "config.json")
     if not os.path.exists(config_path):
         logger.info("配置文件不存在，将使用config-template.json模板")
-        config_path = "./config-template.json"
+        config_path = os.path.join(config_dir, "config-template.jso")
 
     config_str = read_file(config_path)
     logger.debug("[INIT] config str: {}".format(config_str))
--- plugins/plugin_manager.py
+++ plugins/plugin_manager.py
@@ -52,7 +52,9 @@ class PluginManager:
         return wrapper
 
     def save_config(self):
-        with open("./plugins/plugins.json", "w", encoding="utf-8") as f:
+        config_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(config_dir, "plugins.json")
+        with open(config_path, "w", encoding="utf-8") as f:
             json.dump(self.pconf, f, indent=4, ensure_ascii=False)
 
     def load_config(self):
@@ -75,10 +77,11 @@ class PluginManager:
 
     def scan_plugins(self):
         logger.info("Scaning plugins ...")
-        plugins_dir = "./plugins"
+
+        config_dir = os.path.dirname(os.path.abspath(__file__))
         raws = [self.plugins[name] for name in self.plugins]
-        for plugin_name in os.listdir(plugins_dir):
-            plugin_path = os.path.join(plugins_dir, plugin_name)
+        for plugin_name in os.listdir(config_dir):
+            plugin_path = os.path.join(config_dir, plugin_name)
             if os.path.isdir(plugin_path):
                 # 判断插件是否包含同名__init__.py文件
                 main_module_path = os.path.join(plugin_path, "__init__.py")

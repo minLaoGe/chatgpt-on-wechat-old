60ba50c1491093c80a4cb80ef72632420d00ddad
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-05-03 11:00:18 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Replace 'your_flask_script.py' with the name of your Flask script
-FLASK_SCRIPT="app_robot.py"
+# Find Python files with names starting with 'app_robot'
+APP_ROBOT_FILES=$(find . -name "app_robot*.py")
 
-# Find the process ID of the Flask application
-PID=$(pgrep -f $FLASK_SCRIPT)
+# Iterate through the found files
+for FLASK_SCRIPT in $APP_ROBOT_FILES; do
+  # Find the process ID of the Flask application
+  PID=$(pgrep -f $FLASK_SCRIPT)
 
-# If the process ID is not empty, kill the process
-if [ ! -z "$PID" ]; then
-  echo "Killing the Flask app with process ID: $PID"
-  kill -9 $PID
-else
-  echo "Flask app is not running"
-fi
+  # If the process ID is not empty, kill the process
+  if [ ! -z "$PID" ]; then
+    echo "Killing the Flask app with process ID: $PID"
+    kill -9 $PID
+  else
+    echo "Flask app is not running"
+  fi
 
-# Wait for a while to make sure the process is terminated
-sleep 2
+  # Wait for a while to make sure the process is terminated
+  sleep 2
 
-# Start the Flask application
-echo "Starting the Flask app"
-python $FLASK_SCRIPT &
-# Print the process ID of the newly started Flask app
-NEW_PID=$(pgrep -f $FLASK_SCRIPT)
-echo "Flask app started with process ID: $NEW_PID"
-
-
+  # Start the Flask application
+  echo "Starting the Flask app"
+  python $FLASK_SCRIPT &
+  # Print the process ID of the newly started Flask app
+  NEW_PID=$(pgrep -f $FLASK_SCRIPT)
+  echo "Flask app started with process ID: $NEW_PID"
+done

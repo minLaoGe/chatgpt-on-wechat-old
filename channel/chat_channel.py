c11512d21404fad38e91ce294344b2246fa1696e
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-05-05 00:23:32 +0800

    添加日志

--- channel/chat_channel.py
+++ channel/chat_channel.py
@@ -181,7 +181,11 @@ class ChatChannel(Channel):
             )
         )
         reply = e_context["reply"]
-
+        logger.debug(
+            "[WX]  type={}, welcome={}".format(
+                context.type, conf().get("welcome")
+            )
+        )
         if context.type == ContextType.JOIN_GROUP and conf().get("welcome"):
             return conf().get("welcome")
 

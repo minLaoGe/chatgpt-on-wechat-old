6f7ad38c005946edd8319a1b4c2d1a7eca714912
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-05-08 19:22:48 +0800

    再试一次我就不行

--- restart.py
+++ restart.py
@@ -1,7 +1,9 @@
 import os
 from config import conf
+import signal
 
 
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 port = conf().get("flask_port","8082")
 
 home_dir = os.system(f"./start.sh  {port}")

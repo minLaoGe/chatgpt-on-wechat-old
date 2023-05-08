3327b4720afba84aa6ccb8be20e0163c53269c95
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-05-08 19:49:26 +0800

    再试一次我就不行

--- restart.py
+++ restart.py
@@ -1,8 +1,20 @@
 import os
+import subprocess
 from config import conf
+import threading
+import time
 
 
+def script_to_run():
+    port = conf().get("flask_port", "8082")
+    script_name = './start.sh'
 
-port = conf().get("flask_port","8082")
+    # 使用 subprocess.Popen 创建一个新的子进程来执行脚本
+    subprocess.Popen(['bash', script_name, str(port)])
 
-home_dir = os.system(f"./start.sh  {port}")
+
+# 创建一个非守护线程来执行 script_to_run 函数
+non_daemon_thread = threading.Thread(target=script_to_run, daemon=False)
+
+non_daemon_thread.start()
+print("Main thread continues...")

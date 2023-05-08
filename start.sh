ba177f9975a8a2acfaf24690b02a3612a0fcdca1
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-05-08 18:23:02 +0800

    再次修改启动脚本

--- start.sh
+++ start.sh
@@ -9,20 +9,22 @@ APP_ROBOT_FILES=$(find . -name "app_robot*.py")
 
 # Iterate through the found files
 for FLASK_SCRIPT in $APP_ROBOT_FILES; do
+  FILENAME=$(basename $FLASK_SCRIPT)
+
   # Find the process ID of the Flask application
-  PID=$(pgrep -f $FLASK_SCRIPT)
+  PID=$(pgrep -f $FILENAME)
 
   # If the process ID is not empty, kill the process
   if [ ! -z "$PID" ]; then
     echo "Killing the Flask app with process ID: $PID"
-    echo "Killing the Flask appname is: $FLASK_SCRIPT"
+    echo "Killing the Flask appname is: $FILENAME"
     kill -9 $PID
   else
     echo "Flask app is not running"
   fi
 
   # Wait for a while to make sure the process is terminated
-  sleep 29
+  sleep 3
 
   # Start the Flask application
   echo "Starting the Flask app"
